4d0d4ed6b94406256a5e1f02626c7236eae98453
ryaasishlah <ryaasishlah123@gmail.com>
2023-11-03 20:22:19 +0700

    update pase

--- paseto.go
+++ paseto.go
@@ -335,87 +335,17 @@ func GCFLoginAfterCreateeee(MONGOCONNSTRINGENV, dbname, collectionname string, r
 	}
 }
 
-// func GetDataUserFromGCF(PublicKey, MongoEnv, dbname, colname string, r *http.Request) string {
-// 	req := new(ResponseDataUser)
-// 	conn := MongoCreateConnection(MongoEnv, dbname)
-// 	cihuy := new(Response)
-// 	err := jsoncuy.NewDecoder(r.Body).Decode(&cihuy)
-// 	if err != nil {
-// 		req.Status = false
-// 		req.Message = "error parsing application/json: " + err.Error()
-// 	} else {
-// 		checktoken := watoken.DecodeGetId(os.Getenv(PublicKey), cihuy.Token)
-// 		compared := CompareUsername(conn, colname, checktoken)
-// 		if compared != true {
-// 			req.Status = false
-// 			req.Message = "Data Username tidak ada di database"
-// 		} else {
-// 			datauser := GetAllUser(conn, colname)
-// 			req.Status = true
-// 			req.Message = "data User berhasil diambil"
-// 			req.Data = datauser
-// 		}
-// 	}
-// 	return ReturnStringStruct(req)
-// }
-
 // func ReturnStringStruct(Data any) string {
-// 	json, _ := jsoncuy.Marshal(Data)
-// 	return string(json)
+// 	jsonee, _ := json.Marshal(Data)
+// 	return string(jsonee)
 // }
 
-// func GCFPasetoTokenStr(PrivateKey, MongoEnv, dbname, collectionname string, r *http.Request) string {
-// 	var resp Credential
-// 	resp.Status = false
-// 	mconn := MongoCreateConnection(MongoEnv, dbname)
-// 	var datauser User
-// 	err := jsoncuy.NewDecoder(r.Body).Decode(&datauser)
-// 	if err != nil {
-// 		resp.Message = "error parsing application/json: " + err.Error()
-// 	} else {
-// 		if PasswordValidator(mconn, collectionname, datauser) {
-// 			resp.Status = true
-// 			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(PrivateKey))
-// 			if err != nil {
-// 				resp.Message = "Gagal Encode Token : " + err.Error()
-// 			} else {
-// 				resp.Message = "Selamat Datang"
-// 				resp.Token = tokenstring
-// 			}
-// 		} else {
-// 			resp.Message = "Password Salah"
-// 		}
-// 	}
-
-// 	return ReturnStringStruct(resp)
-// }
-
-// func GCFPasswordHasher(r *http.Request) string {
+// func Register(Mongoenv, dbname string, r *http.Request) string {
 // 	resp := new(Credential)
-// 	userdata := new(User)
+// 	userdata := new(RegisterStruct)
 // 	resp.Status = false
-// 	err := jsoncuy.NewDecoder(r.Body).Decode(&userdata)
-// 	if err != nil {
-// 		resp.Message = "error parsing application/json: " + err.Error()
-// 	} else {
-// 		passwordhash, err := HashPass(userdata.Password)
-// 		if err != nil {
-// 			resp.Message = "Gagal Hash Passwordnya : " + err.Error()
-// 		} else {
-// 			resp.Status = true
-// 			resp.Message = "Berhasil Hash Password"
-// 			resp.Token = passwordhash
-// 		}
-// 	}
-// 	return ReturnStringStruct(resp)
-// }
-
-// func InsertDataUserGCF(Mongoenv, dbname string, r *http.Request) string {
-// 	resp := new(Credential)
-// 	userdata := new(User)
-// 	resp.Status = false
-// 	conn := MongoCreateConnection(Mongoenv, dbname)
-// 	err := jsoncuy.NewDecoder(r.Body).Decode(&userdata)
+// 	conn := GetConnectionMongo(Mongoenv, dbname)
+// 	err := json.NewDecoder(r.Body).Decode(&userdata)
 // 	if err != nil {
 // 		resp.Message = "error parsing application/json: " + err.Error()
 // 	} else {
@@ -424,8 +354,33 @@ func GCFLoginAfterCreateeee(MONGOCONNSTRINGENV, dbname, collectionname string, r
 // 		if err != nil {
 // 			resp.Message = "Gagal Hash Password" + err.Error()
 // 		}
-// 		InsertUserdata(conn, userdata.Username, userdata.Role, hash)
+// 		InsertUserdata(conn, userdata.Username, hash)
 // 		resp.Message = "Berhasil Input data"
+// 	}
+// 	response := pasproj.ReturnStringStruct(resp)
+// 	return response
+// }
+
+// func Login(Privatekey, MongoEnv, dbname, Colname string, r *http.Request) string {
+// 	var resp Credential
+// 	mconn := MongoCreateConnection(MongoEnv, dbname)
+// 	var datauser peda.User
+// 	err := json.NewDecoder(r.Body).Decode(&datauser)
+// 	if err != nil {
+// 		resp.Message = "error parsing application/json: " + err.Error()
+// 	} else {
+// 		if peda.IsPasswordValid(mconn, Colname, datauser) {
+// 			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(Privatekey))
+// 			if err != nil {
+// 				resp.Message = "Gagal Encode Token : " + err.Error()
+// 			} else {
+// 				resp.Status = true
+// 				resp.Message = "Selamat Datang"
+// 				resp.Token = tokenstring
+// 			}
+// 		} else {
+// 			resp.Message = "Password Salah"
+// 		}
 // 	}
 // 	return ReturnStringStruct(resp)
 // }
