b633feb86193fe5d5d98f1b61364168bcdad448c
ryaasishlah <ryaasishlah123@gmail.com>
2023-11-03 20:14:07 +0700

    add registerstruct

--- type.go
+++ type.go
@@ -21,3 +21,13 @@ type Response struct {
 	Message string      `json:"message" bson:"message"`
 	Data    interface{} `json:"data" bson:"data"`
 }
+
+type Credents struct {
+	Status  string `json:"status" bson:"status"`
+	Message string `json:"message" bson:"message"`
+}
+
+type RegisterStruct struct {
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
+}
