2b15b678f77becfc6a53b6eecd3ced4a9ae0acec
ryaasishlah <ryaasishlah123@gmail.com>
2023-11-06 18:20:13 +0700

    update modul

---
+++ modul/controller.go
@@ -0,0 +1,285 @@
+package modul
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/badoux/checkmail"
+	model "github.com/pasabar/pasetobackendpasabar/model"
+)
+
+func MongoConnect(MongoString, dbname string) *mongo.Database {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv(MongoString)))
+	if err != nil {
+		fmt.Printf("MongoConnect: %v\n", err)
+	}
+	return client.Database(dbname)
+}
+
+func InsertOneDoc(db *mongo.Database, col string, docs interface{}) (insertedID primitive.ObjectID, err error) {
+	cols := db.Collection(col)
+	result, err := cols.InsertOne(context.Background(), docs)
+	if err != nil {
+		fmt.Printf("InsertOneDoc: %v\n", err)
+	}
+	insertedID = result.InsertedID.(primitive.ObjectID)
+	return
+}
+
+func UpdateOneDoc(db *mongo.Database, col string, filter bson.M, update bson.M) (err error) {
+	cols := db.Collection(col)
+	_, err = cols.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		fmt.Printf("UpdateOneDoc: %v\n", err)
+	}
+	return
+}
+
+func GetOneDoc(db *mongo.Database, col string, filter bson.M, docs interface{}) interface{} {
+	collection := db.Collection(col)
+	err := collection.FindOne(context.Background(), filter).Decode(&docs)
+	if err != nil {
+		fmt.Printf("GetOneDoc: %v\n", err)
+	}
+	return docs
+}
+
+func GetAllDocs(db *mongo.Database, col string, docs interface{}) interface{} {
+	cols := db.Collection(col)
+	filter := bson.M{}
+	cursor, err := cols.Find(context.TODO(), filter)
+	if err != nil {
+		fmt.Println("Error GetAllDocs in colection", col, ":", err)
+	}
+	err = cursor.All(context.TODO(), &docs)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return docs
+}
+
+// user
+func Register(db *mongo.Database, col string, userdata model.User) error {
+	if userdata.Username == "" || userdata.Password == "" || userdata.Email == "" {
+		return fmt.Errorf("Data tidak lengkap")
+	}
+
+	// Periksa apakah email valid
+	if err := checkmail.ValidateFormat(userdata.Email); err != nil {
+		return fmt.Errorf("Email tidak valid")
+	}
+
+	// Periksa apakah email dan username sudah terdaftar
+	userExists, _ := GetUserFromEmail(db, col, userdata.Email)
+	if userExists.Email != "" {
+		return fmt.Errorf("Email sudah terdaftar")
+	}
+	userExists, _ = GetUserFromUsername(db, col, userdata.Username)
+	if userExists.Username != "" {
+		return fmt.Errorf("Username sudah terdaftar")
+	}
+
+	// Periksa apakah password memenuhi syarat
+	if len(userdata.Password) < 6 {
+		return fmt.Errorf("Password minimal 6 karakter")
+	}
+	if strings.Contains(userdata.Password, " ") {
+		return fmt.Errorf("Password tidak boleh mengandung spasi")
+	}
+
+	// Periksa apakah username memenuhi syarat
+	if strings.Contains(userdata.Username, " ") {
+		return fmt.Errorf("Username tidak boleh mengandung spasi")
+	}
+
+	// Simpan pengguna ke basis data
+	hash, _ := HashPassword(userdata.Password)
+	user := bson.M{
+		"_id":      primitive.NewObjectID(),
+		"email":    userdata.Email,
+		"username": userdata.Username,
+		"password": hash,
+		"role":     "user",
+	}
+	_, err := InsertOneDoc(db, col, user)
+	if err != nil {
+		return fmt.Errorf("SignUp: %v", err)
+	}
+	return nil
+}
+
+func LogIn(db *mongo.Database, col string, userdata model.User) (user model.User, status bool, err error) {
+	if userdata.Username == "" || userdata.Password == "" {
+		err = fmt.Errorf("Data tidak lengkap")
+		return user, false, err
+	}
+
+	// Periksa apakah pengguna dengan username tertentu ada
+	userExists, _ := GetUserFromUsername(db, col, userdata.Username)
+	if userExists.Username == "" {
+		err = fmt.Errorf("Username tidak ditemukan")
+		return user, false, err
+	}
+
+	// Periksa apakah kata sandi benar
+	if !CheckPasswordHash(userdata.Password, userExists.Password) {
+		err = fmt.Errorf("Password salah")
+		return user, false, err
+	}
+	return userExists, true, nil
+}
+
+func ChangePassword(db *mongo.Database, col string, username string, oldPassword string, newPassword string) (user model.User, status bool, err error) {
+	if username == "" || newPassword == "" || oldPassword == "" {
+		err = fmt.Errorf("Data tidak lengkap")
+		return user, false, err
+	}
+
+	// Periksa apakah pengguna dengan username tertentu ada
+	userExists, err := GetUserFromUsername(db, col, username)
+	if err != nil {
+		return user, false, err
+	}
+	if userExists.Username == "" {
+		err = fmt.Errorf("Username tidak ditemukan")
+		return user, false, err
+	}
+
+	// Periksa apakah kata sandi lama benar
+	if !CheckPasswordHash(oldPassword, userExists.Password) {
+		err = fmt.Errorf("Kata sandi lama salah")
+		return user, false, err
+	}
+
+	// Periksa apakah kata sandi baru memenuhi syarat
+	if len(newPassword) < 6 {
+		err = fmt.Errorf("Password minimal 6 karakter")
+		return user, false, err
+	}
+	if strings.Contains(newPassword, " ") {
+		err = fmt.Errorf("Password tidak boleh mengandung spasi")
+		return user, false, err
+	}
+
+	// Periksa apakah kata sandi baru berbeda dengan kata sandi lama
+	if oldPassword == newPassword {
+		err = fmt.Errorf("Kata sandi baru harus berbeda dengan kata sandi lama")
+		return user, false, err
+	}
+
+	// Ubah kata sandi pengguna
+	newPasswordHash, err := HashPassword(newPassword)
+	if err != nil {
+		return user, false, err
+	}
+
+	userExists.Password = newPasswordHash
+
+	// Simpan perubahan kata sandi ke basis data
+	err = UpdateOneDoc(db, col, bson.M{"_id": userExists.ID}, bson.M{"$set": bson.M{"password": newPasswordHash}})
+	if err != nil {
+		if err != nil {
+			return user, false, err
+		}
+		return userExists, true, nil
+	}
+	return userExists, true, nil
+}
+
+func DeleteUser(db *mongo.Database, col string, username string) error {
+	cols := db.Collection(col)
+	filter := bson.M{"username": username}
+	result, err := cols.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return fmt.Errorf("error deleting data for Username %s: %s", username, err.Error())
+	}
+	if result.DeletedCount == 0 {
+		return fmt.Errorf("no data found for Username %s", username)
+	}
+	return nil
+}
+
+func GetUserFromID(db *mongo.Database, col string, _id primitive.ObjectID) (user model.User, err error) {
+	cols := db.Collection(col)
+	filter := bson.M{"_id": _id}
+	err = cols.FindOne(context.TODO(), filter).Decode(&user)
+	if err != nil {
+		fmt.Printf("GetUserFromID: %v\n", err)
+	}
+	return user, nil
+}
+
+func GetUserFromUsername(db *mongo.Database, col string, username string) (user model.User, err error) {
+	cols := db.Collection(col)
+	filter := bson.M{"username": username}
+	err = cols.FindOne(context.TODO(), filter).Decode(&user)
+	if err != nil {
+		fmt.Printf("GetUserFromUsername: %v\n", err)
+		return user, err
+	}
+	return user, nil
+}
+
+func GetUserFromEmail(db *mongo.Database, col string, email string) (user model.User, err error) {
+	cols := db.Collection(col)
+	filter := bson.M{"email": email}
+	err = cols.FindOne(context.Background(), filter).Decode(&user)
+	if err != nil {
+		fmt.Printf("GetUserFromEmail: %v\n", err)
+	}
+	return user, nil
+}
+
+func GetAllUser(db *mongo.Database, col string) (userlist []model.User) {
+	cols := db.Collection(col)
+	filter := bson.M{}
+	cursor, err := cols.Find(context.TODO(), filter)
+	if err != nil {
+		fmt.Println("Error GetAllDocs in colection", col, ":", err)
+	}
+	err = cursor.All(context.TODO(), &userlist)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return userlist
+}
+
+// catalog
+func InsertCatalog(db *mongo.Database, col string, catalog model.Catalog) (insertedID primitive.ObjectID, err error) {
+	insertedID, err = InsertOneDoc(db, col, catalog)
+	if err != nil {
+		fmt.Printf("InsertCatalog: %v\n", err)
+	}
+	return insertedID, err
+}
+
+func GetCatalogFromID(db *mongo.Database, col string, id primitive.ObjectID) (catalog model.Catalog) {
+	cols := db.Collection(col)
+	filter := bson.M{"_id": id}
+	err := cols.FindOne(context.Background(), filter).Decode(&catalog)
+	if err != nil {
+		fmt.Printf("GetCatalogFromID: %v\n", err)
+	}
+	return catalog
+}
+
+func GetCatalogList(db *mongo.Database, col string) (cataloglist model.CatalogList) {
+	cols := db.Collection(col)
+	filter := bson.M{}
+	cursor, err := cols.Find(context.Background(), filter)
+	if err != nil {
+		fmt.Println("Error GetAllDocs in colection", col, ":", err)
+	}
+	err = cursor.All(context.Background(), &cataloglist.Items)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return cataloglist
+}
---
+++ modul/handler.go
@@ -0,0 +1,117 @@
+package modul
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strconv"
+
+	model "github.com/pasabar/pasetobackendpasabar/model"
+	"github.com/whatsauth/watoken"
+)
+
+func GCFHandler(MONGOCONNSTRINGENV, dbname, col string) string {
+	mconn := MongoConnect(MONGOCONNSTRINGENV, dbname)
+	data := GetAllUser(mconn, col)
+	return GCFReturnStruct(data)
+}
+
+func GCFHandlerGetUser(MONGOCONNSTRINGENV, dbname, col string, username string) string {
+	mconn := MongoConnect(MONGOCONNSTRINGENV, dbname)
+	data, err := GetUserFromUsername(mconn, col, username)
+	if err != nil {
+		return GCFReturnStruct(err)
+	}
+	return GCFReturnStruct(data)
+}
+
+func GCFHandlerRegister(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	var Response model.Credential
+	Response.Status = false
+	mconn := MongoConnect(MONGOCONNSTRINGENV, dbname)
+	var datauser model.User
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+	}
+	err = Register(mconn, collectionname, datauser)
+	if err != nil {
+		Response.Message = err.Error()
+		return GCFReturnStruct(Response)
+	}
+	Response.Status = true
+	Response.Message = "Register success" + datauser.Username
+
+	return GCFReturnStruct(Response)
+}
+
+func GCFHandlerLogIn(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	var Response model.Credential
+	Response.Status = false
+	mconn := MongoConnect(MONGOCONNSTRINGENV, dbname)
+	var datauser model.User
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+	}
+	user, status, err := LogIn(mconn, collectionname, datauser)
+	if err != nil {
+		Response.Message = err.Error()
+		return GCFReturnStruct(Response)
+	}
+	Response.Status = true
+	tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(PASETOPRIVATEKEYENV))
+	if err != nil {
+		Response.Message = "Gagal Encode Token :" + err.Error()
+	} else {
+		Response.Message = "Login success" + user.Username + strconv.FormatBool(status)
+		Response.Token = tokenstring
+	}
+
+	return GCFReturnStruct(Response)
+}
+
+func GCFHandlerChangePassword(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	var Response model.Credential
+	Response.Status = false
+	mconn := MongoConnect(MONGOCONNSTRINGENV, dbname)
+	var datauser model.User
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+	}
+	userdata, status, err := ChangePassword(mconn, collectionname, datauser.Username, datauser.Password, datauser.Password)
+	if err != nil {
+		Response.Message = err.Error()
+		return GCFReturnStruct(Response)
+	}
+	Response.Status = true
+	Response.Message = "Password change success for user" + userdata.Username + strconv.FormatBool(status)
+
+	return GCFReturnStruct(Response)
+}
+
+func GCFHandlerDeleteUser(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	var Response model.Credential
+	Response.Status = false
+	mconn := MongoConnect(MONGOCONNSTRINGENV, dbname)
+	var datauser model.User
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+	}
+	err = DeleteUser(mconn, collectionname, datauser.Username)
+	if err != nil {
+		Response.Message = err.Error()
+		return GCFReturnStruct(Response)
+	}
+	Response.Status = true
+	Response.Message = "Delete user success"
+
+	return GCFReturnStruct(Response)
+}
+
+func GCFReturnStruct(DataStuct any) string {
+	jsondata, _ := json.Marshal(DataStuct)
+	return string(jsondata)
+}
